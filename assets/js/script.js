09ff07c94f89f76e630f63cfe6685565bfb07ce4
Dixie Waigel <dixiewaigel@me.com>
2020-12-02 11:29:08 -0800

    tea time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,30 @@
-new TypeIt("#greeting", {
-    strings: "Hello. &#127803;",
-    speed: 175,
-    loop: true
-  }).go();
+new TypeIt("#offering", {
+    speed: 80,
+    cursor: false,
+    lifeLike: true,
+    waitUntilVisible: true
+  })
+    .pause(4000)
+    .type("Here. ")
+    .pause(400)
+    .type("Have some matcha.")
+    .go();
+
+new TypeIt("#breathe", {
+    speed: 80,
+    lifeLike: true,
+    cursor: false,
+    waitUntilVisible: true
+  })
+    .pause(7500)
+    .type("Breathe in. ", {delay: 300})
+    .pause(2000)
+    .delete(12)
+    .pause(500)
+    .type("Breathe out.")
+    .pause(2500)
+    .delete(12)
+    .pause(500)
+    .type("Let's talk about tea.")
+    .go();
+
